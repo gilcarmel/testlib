1ee4fc0ba64cb05356aa23c19f6f63f1f4dbdbbb
Gil Carmel <gil.carmel@gmail.com>
2016-03-18 11:01:16 -0700

    silly class

---
+++ app/src/main/java/com/gilcarmel/testlib/SillyClass.java
@@ -0,0 +1,12 @@
+package com.gilcarmel.testlib;
+
+import android.util.Log;
+
+/**
+ * Created by gil on 3/18/16.
+ */
+public class SillyClass {
+    static public void printStuff() {
+        Log.d("Testlib", "Printing");
+    }
+}
